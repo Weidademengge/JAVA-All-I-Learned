6d8100296ef463a36298670e77836367405fd931
WDMG\duym <13041376587@163.com>
2023-03-09 17:19:33 +0800

    test分支第二次提交测试

---
+++ leetcode/src/main/java/com/wddmg/leetcode/leetcode1/Test2.java
@@ -0,0 +1,31 @@
+package com.wddmg.leetcode.leetcode1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. 两数之和
+ * esay
+ * 双循环遍历
+ * 25.9%;81.66%
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums,target)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{i,map.get(target- nums[i])};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
